6f2a691c0a771ea927e68f828e7efe290c111cbe
Cristofer <cristofergm123@gmail.com>
2021-11-21 17:10:52 -0400

    arreglando problemas

--- cliente/src/Proyecto/Admin/paginas/materias/Materias.js
+++ cliente/src/Proyecto/Admin/paginas/materias/Materias.js
@@ -29,17 +29,43 @@ export const Materias = () => {
                     <div className="col-7">
                         <h2 className="mb-3 mt-3 text-muted">Materias</h2>
                         <form onSubmit={enviarDatos}>
-                            <div class="mb-3 mt-3">
+                            <div className="mb-3 mt-3">
                                 <label for="materia" className="form-label">Nombre de la materia:</label>
                                 <input type="text" name="nombre" className="form-control" placeholder="Ej. Claculo" onChange={valorInpunt}/>
                             </div>
-                            <div class="mb-3">
+                            <div className="mb-3">
                                 <label for="hora" className="form-label">Cargas horarias en la semana:</label>
                                 <input type="number" name="horas" className="form-control" placeholder="Ej. 6" onChange={valorInpunt}/>
                             </div>
-                            <button type="submit" className="btn btn-primary">Enviar</button>
+                            <button type="submit" className="btn btn-primary mb-5 mt-3">Enviar</button>
                         </form>
                     </div>
+                </div>
+                <div className="table-responsive">
+                    <table className="table table-bordered table-hover">
+                        <thead className="table-primary">
+                            <tr>
+                                <th>No</th>
+                                <th>Materia</th>
+                                <th>Horas</th>
+                                <th>Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>7:45 - 10:00</td>
+                                <td>Programacion Amb2 PB</td>
+                                <td></td>
+                                <td>Metodologia Amb3 1er piso</td>
+                            </tr>
+                            <tr>
+                                <td>10:00-12:15</td>
+                                <td></td>
+                                <td>Programacion Amb2 PB</td>
+                                <td></td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </>
