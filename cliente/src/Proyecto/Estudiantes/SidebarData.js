80203a178677e9d6e79bdf3926e79b64ff2447f2
marisabel flores kacka <floreskackamarisabel@gmail.com>
2021-11-22 16:58:54 -0400

    estructura

---
+++ cliente/src/Proyecto/Estudiantes/SidebarData.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const SidebarData=  [
+     {
+         title: 'Programar Materias',
+         path: '/',
+         cName: 'nav-text',
+     },
+     {
+        title: 'Ver Programacion',
+        path: '/',
+        cName: 'nav-text',
+    },
+]
+export default SidebarData;
--- cliente/src/Proyecto/RutasApp.js
+++ cliente/src/Proyecto/RutasApp.js
@@ -2,29 +2,35 @@ import React from 'react'
 import {
     BrowserRouter as Router,
     Switch,
-    Route,
-    Link
+    Route
   } from "react-router-dom";
-import { NavBar } from './Admin/componentes/NavBar';
-import { Ambientes } from './Admin/paginas/ambientes/Ambientes';
-import { Materias } from './Admin/paginas/materias/Materias';
-import Docentes from './Docente/paginas/Docentes';
-import verHorarios from './Docente/paginas/horarios/verHorarios';
-import Estudiantes from './Estudiante/paginas/Estudiantes';
+import { Administrador } from './Admin/Administrador';
+import { Ambientes } from './Admin/ambientes/Ambientes';
+import listaDocentes from './Admin/paginas/listaDocentes';
+import programarMaterias from './Admin/paginas/programarMaterias';
+import { Docentes } from './Docentes/Docentes';
+import verHorarios from './Docentes/paginas/verHorarios';
+import { Estudiantes } from './Estudiantes/Estudiantes';
+
 import { home } from './home';
 
 
 export const RutasApp = () => {
     return (
         <Router>
-            <NavBar/>
+            <h3>Sistema de Gestion de Horarios </h3>
+            
+              <div className="navbar-z">
+                </div>
             <Switch>
                <Route exact path="/" component={home}/>
-                <Route path="/materias" component={Materias}/>
                 <Route path="/Estudiantes" component={Estudiantes}/>
                 <Route path="/Docentes" component={Docentes}/>
+                <Route path="/Administrador" component={Administrador}/>
                 <Route path="/verHorarios" component={verHorarios}/>
-                <Route path="/ambientes" component={Ambientes}/>
+                <Route path="/Ambientes" component={Ambientes}/>
+                <Route path="/programarMaterias" component={programarMaterias}/>
+                <Route path="/listaDocentes" component={listaDocentes}/>
             </Switch>
         </Router>
     )
